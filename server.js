c47ceac17b824903bf5ec3dd2f09707bd19c139c
Osku <osamah.mohammed@knowit.fi>
2022-05-24 14:55:59 +0300

    Enable statics files

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const genresRoute = require("./routes/genres");
-
+/* Middle wares */
 app.use(express.json());
 app.use(genresRoute);
-
+app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
